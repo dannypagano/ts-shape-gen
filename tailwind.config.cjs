e261cc9c8a0c29fdb9caba6b39cf864b2aea5a1e
Danny Pagano <danny@tailscale.com>
2025-10-22 16:16:49 -0700

    fix deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,12 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    // Preline components (if you use any)
     "./node_modules/preline/dist/*.js",
   ],
   theme: { extend: {} },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
-    require("@preline/plugin"), // <— updated
+    require("@preline/plugin"),
   ],
 };
