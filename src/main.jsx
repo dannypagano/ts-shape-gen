dc2e609c2c7fddb3419f773234160122a5da4ae0
Danny Pagano <danny@tailscale.com>
2025-10-23 18:51:02 -0700

    re-order imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App.jsx";
 import "preline/preline";
-import "./index.css";
 
 // Optional, safe polyfill for older Safari/Firefox engines
 (async () => {
